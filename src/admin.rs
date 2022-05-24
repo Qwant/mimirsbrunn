6495722e59a22da2dd5af851b42a9b2409f7f77b
sdirollo <sebastien.dirollo@gmail.com>
2022-05-24 11:59:05 +0200

    fix build

--- src/admin.rs
+++ src/admin.rs
@@ -45,7 +45,6 @@ use mimir::{
     domain::ports::primary::generate_index::GenerateIndex,
 };
 use places::admin::Admin;
-use places::i18n_properties::I18nProperties;
 
 #[derive(Debug, Snafu)]
 pub enum Error {
@@ -125,22 +124,6 @@ fn get_alternative_label_name(
     return alternative_label_name;
 }
 
-// fn merge_i18n_names(
-//     tags: &osmpbfreader::Tags,
-//     center_tags: &osmpbfreader::Tags,
-//     label: &str,
-//     langs: &[String],
-// ) -> I18nProperties {
-//     let mut alt_names = osm_utils::get_label_languages_from_tags(tags, label, langs);
-//     let center_alt_names = osm_utils::get_label_languages_from_tags(center_tags, label, langs);
-//     center_alt_names.0.into_iter().for_each(|prop| {
-//         if !alt_names.0.contains(&prop) {
-//             alt_names.0.push(prop)
-//         }
-//     });
-//     return alt_names;
-// }
-
 impl IntoAdmin for Zone {
     fn into_admin(
         self,
@@ -197,8 +180,8 @@ impl IntoAdmin for Zone {
                 .collect(),
             codes,
             names: osm_utils::get_label_languages_from_tags(&self.tags, "name:", langs),
-            alt_names: osm_utils::get_label_languages_from_tags(tags, "alt_name:", langs),
-            loc_names: osm_utils::get_label_languages_from_tags(tags, "loc_name:", langs),
+            alt_names: osm_utils::get_label_languages_from_tags(&self.tags, "alt_name:", langs),
+            loc_names: osm_utils::get_label_languages_from_tags(&self.tags, "loc_name:", langs),
             labels: self
                 .international_labels
                 .into_iter()
