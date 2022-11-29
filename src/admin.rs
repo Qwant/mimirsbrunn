eda5a974cd54ab118b83948b39f02cfe94e4772b
sdirollo <sebastien.dirollo@gmail.com>
2022-11-29 17:13:14 +0100

    concatenate string

--- src/admin.rs
+++ src/admin.rs
@@ -179,12 +179,7 @@ impl IntoAdmin for Zone {
             names: osm_utils::get_label_languages_from_tags(&self.tags, "name:", langs),
             alt_names: osm_utils::get_label_languages_from_tags(&self.tags, "alt_name:", langs),
             loc_names: osm_utils::get_label_languages_from_tags(&self.tags, "loc_name:", langs),
-            full_label_extra: vec![
-                "le".to_string(),
-                "la".to_string(),
-                "les".to_string(),
-                "l'".to_string(),
-            ],
+            full_label_extra: vec![format!("{}{}{}{}", "le", "la", "les", "l'")],
             labels: self
                 .international_labels
                 .into_iter()
