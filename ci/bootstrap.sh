c1da8ec27c8d6d49a0cbadf0c2b36480e393882c
Davide Madrisan <d.madrisan@qwant.com>
2023-06-16 15:12:06 +0200

    CI-1518 Build images with kaniko and k8s runners
    Signed-off-by: Davide Madrisan <d.madrisan@qwant.com>

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 set -euo pipefail
-shopt -s expand_aliases
- 
-source ci/bootstrap/src/bootstrap.sh
+type -p shopt && shopt -s expand_aliases
+
+source ci/bootstrap/src/k8s/bootstrap.sh
+
 export PATH="$PATH:$PWD/ci/bootstrap/bin"
 export no_proxy="$no_proxy,elasticsearch"
 export NO_PROXY="$NO_PROXY,elasticsearch"
