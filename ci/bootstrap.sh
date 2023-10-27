5d6ec6171170597608cf3b25613571ae9266418e
Paul Delafosse <paul.delafosse@protonmail.com>
2023-10-27 14:30:27 +0200

    chore: remove gitlab leftovers

--- ci/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-set -euo pipefail
-type -p shopt && shopt -s expand_aliases
-
-source ci/bootstrap/src/k8s/bootstrap.sh
-
-export PATH="$PATH:$PWD/ci/bootstrap/bin"
-export no_proxy="$no_proxy,elasticsearch"
-export NO_PROXY="$NO_PROXY,elasticsearch"
